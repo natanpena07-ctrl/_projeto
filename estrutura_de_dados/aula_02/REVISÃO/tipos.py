a7248c75843abd469d942eee83211d52c3f56195
Natan Bernado <natanpena07@gmail.com>
2025-08-20 10:46:17 -0300

    subindo primeira versão

--- estrutura_de_dados/aula_02/REVISÃO/tipos.py
+++
@@ -1,5 +0,0 @@
-produto = "Televisão" # coloquei entre aspas, ele entende como string
-quantidade = 25 # variável tipo number - inteiro
-preco = 100.00 # variável tipo number - decimal
-emEstoque = True # variável tipo boolean
-POSSUICARTAO = True # variável tipo constante fake
